1cbf24177c89795216634be159b8e1f6d8deebd4
yasper <yasperrus@gmail.com>
2026-04-29 05:51:09 +0300

    wrong name def get_groups fixed on get_group_templates

--- core/services/sync/pull/builder.py
+++ core/services/sync/pull/builder.py
@@ -3,7 +3,7 @@ from django.utils.timezone import now
 from .blocks import get_blocks
 from .blocktasks import get_blocktasks
 from .templates import get_templates
-from .groups import get_groups
+from .groups import get_group_templates
 from .system_templates import get_system_templates
 from .deleted import get_deleted
 
@@ -14,7 +14,7 @@ def build_pull_response(request, last_sync):
         "blocks": get_blocks(request, last_sync),
         "tasks": get_blocktasks(request, last_sync),
         "templates": get_templates(request, last_sync),
-        "groups": get_groups(request, last_sync),
+        "groups": get_group_templates(request, last_sync),
         "system_templates": get_system_templates(request, last_sync),
         "deleted": get_deleted(request, last_sync),
     }
